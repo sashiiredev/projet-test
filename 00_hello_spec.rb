776bb002969f3c8c851f318e45d66da3c754499a
sashiiredev <55735863+sashiiredev@users.noreply.github.com>
2019-10-07 22:57:25 +0200

    Add files via upload

---
+++ 00_hello_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/00_hello'
+
+describe "the hello function" do
+  it "says hello" do
+    expect(hello).to eq("Hello world!")
+  end
+end
+
+ describe "the greet function" do 
+  it "says hello to someone" do
+     expect(greet("Alice")).to eq("Hello, Alice!")
+ 
+ end
+
+  it "says hello to someone else" do
+     expect(greet("Bob")).to eq("Hello, Bob!")
+  end
+end
